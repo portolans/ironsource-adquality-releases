0371ac2af946fde0c61b7ad7593c6d791157b88b
Portolabot <portolabot@hiportola.com>
2026-01-26 06:11:36 +0000

    Update to 9.2.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "IronSourceAdQualitySDK",
             url: "https://github.com/portolans/ironsource-adquality-releases/releases/download/9.2.1/IronSourceAdQualitySDK.xcframework.zip",
-            checksum: "4e14505938d2a6180b057343a36aadc7621c3deee6f8e39ca75e2f9b112f2576",
+            checksum: "b87388b58111012cd78eb9894d3984e1959729fb71a84e4cf94448163ad61ed9",
         ),
     ],
 )
